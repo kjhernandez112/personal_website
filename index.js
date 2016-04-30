8c977fffbcbda7ff1bee34f663018201f26309e0
Karen J Hernandez <karenjhernandez@gmail.com>
2016-04-30 16:00:25 -0400

    added new content play game

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$("#submit").click(function() {
+var name = $("#name").val();
+var email = $("#email").val();
+var message = $("#message").val();
+var contact = $("#contact").val();
+$("#returnmessage").empty(); // To empty previous error/success message.
+// Checking for blank fields.
+if (name == '' || email == '' || contact == '') {
+alert("Please Fill Required Fields");
+} else {
+// Returns successful data submission message when the entered information is stored in database.
